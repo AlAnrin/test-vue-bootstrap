3b32feee6aec5410d33f1fc21cafe3712b736fca
SIENA\lian <lianirvi@gmail.com>
2020-11-04 19:20:50 +0300

    initial

---
+++ src/components/Test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+
+    <b-card title="Card Title" no-body>
+      <b-card-header header-tag="nav">
+        <b-nav card-header tabs justified >
+          <b-nav-item active>Active</b-nav-item>
+          <b-nav-item>Link</b-nav-item>
+          <b-nav-item>Another Link</b-nav-item>
+          <b-nav-item disabled>Disabled</b-nav-item>
+        </b-nav>
+      </b-card-header>
+
+      <b-card-body class="text-center">
+        <b-card-text>
+          {{msg}}
+        </b-card-text>
+
+        <b-button variant="primary">Go somewhere</b-button>
+      </b-card-body>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
