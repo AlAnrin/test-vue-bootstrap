b4f82a210ebe9608fdcfa3df69b999cda42d9fe6
AlAnrin <lianirvi@gmail.com>
2020-11-05 12:26:40 +0300

    add store & link to simple-nodejs-rest

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -11,5 +12,6 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/store.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        baseUrl: 'http://127.0.0.1:7000',
+    },
+    actions: {
+
+    },
+    mutations: {
+
+    },
+    plugins: [
+
+    ]
+})
